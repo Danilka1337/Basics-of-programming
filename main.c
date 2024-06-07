66391830bfeb371b45b3169f4540dbc716c67c17
Danilka1337 <168913901+Danilka1337@users.noreply.github.com>
2024-06-07 11:31:44 +0300

    1 proverka
    checking  for minimality

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <limits.h>
 
+#define ROWS 4
+#define COLS 4
+
+void findSaddlePoints(int matrix[ROWS][COLS], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            int current = matrix[i][j];
+            int isMinInRow = 1;
+            int isMaxInCol = 1;
+            
+            // Проверка на минимальность в строке
+            for (int k = 0; k < cols; k++) {
+                if (matrix[i][k] < current) {
+                    isMinInRow = 0;
+                    break;
+                }
+            }
+            
